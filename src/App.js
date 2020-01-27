4b01278608d4aebccf05c884956ead6d6516a290
Daniel Eduardo Solis Can <daniel.solis@municipio.merida.gob.mx>
2020-01-27 08:46:37 -0600

    Primera subida

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Navigation from './components/Navigation';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Navigation  titulo="Ayuntamiento de Mérida"/>        
+        
+        <img src={logo} className="App-logo" alt="logo" />        
+      </div>
+    ); 
+  }
+}
+
+export default App;
